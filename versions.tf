8ca5e64253ad8cbbe47c42f3a72dcea2c27bb096
GabrielHespanhol <gabriellopeshespanhol@gmail.com>
2025-10-14 23:07:13 -0300

    chore: adding provider aws default region

--- versions.tf
+++ versions.tf
@@ -4,7 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6"
+      version = "~> 5.0"
     }
   }
+}
+
+provider "aws" {
+    region = "us-east-1"
 }
