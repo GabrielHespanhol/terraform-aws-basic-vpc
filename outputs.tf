9f909b7c73070f260c9911fc5ad52b24f8c39986
GabrielHespanhol <gabriellopeshespanhol@gmail.com>
2025-10-16 22:16:43 -0300

    feat: add new outputs

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 # Module outputs
-output "aws_vpc_cidr" {
+output "aws_vpc_cidr_block" {
   value = var.aws_vpc_cidr_block
 }
 
@@ -9,4 +9,8 @@ output "aws_vpc_name" {
 
 output "this_vpc_arn" {
   value = aws_vpc.main.arn
+}
+
+output "this_vpc_id" {
+  value = aws_vpc.main.id
 }
